aa1a47a98384228e026992481a8358b04c650101
ulissessoaresmoreira <ulissessm@gmail.com>
2021-11-16 10:21:04 +0000

    Teste Event Class ALL

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// let nomeDaVariavelQualquer = document.querySelector('.nomeDaClasse')
+// nomeDaVariavelQualquer.classList.add('string com o nome da classe que quero adicionar')
+
+let notePad = document.querySelectorAll('.unit')
+
+function openNotePad(){
+    notePad.classList.add('teste')
+}
+
